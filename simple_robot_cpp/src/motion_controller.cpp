fd250ee3bb6c4f2b8b8ca0502f55cf66ad3621dc
Mohamad Moudallal <mohamad@moudallal.xyz>
2023-04-05 01:50:35 +0300

    Added C++ version

---
+++ simple_robot_cpp/src/motion_controller.cpp
@@ -0,0 +1,56 @@
+// motion_controller.cpp
+// Author: Mohamad Moudallal
+// GitHub: https://github.com/moudallal
+// Email: mohamad@moudallal.xyz
+
+#include "ros/ros.h"
+
+#include "geometry_msgs/Twist.h"
+#include "simple_robot_cpp/Obstacle.h"
+
+// Initialize publisher and subscriber
+ros::Publisher publisher;
+ros::Subscriber subscriber;
+
+// Subscriber callback method
+void callback(const simple_robot_cpp::Obstacle data) {
+    // Move the robot forward
+    geometry_msgs::Twist move;
+    move.angular.z = 0;
+    move.linear.x = 0.3;
+
+    // Evaluate the distance between the robot and the obstacle
+    if (data.distance < 1) {
+        ROS_INFO("%s detected at %1.3f meters away!", data.name.c_str(), data.distance);
+        ROS_INFO("SEARCHING FOR SAFE PATH");
+        //  Rotate the robot to the left to find a safe path
+        move.linear.x = 0;
+        move.angular.z = 0.5;
+    } else if (isinf(data.distance)) {
+        ROS_INFO("No obstacles detected.");
+        ROS_INFO("MOVING FORWARD");
+    } else {
+        ROS_INFO("Obstacle is still far. Distance: %1.3f meters", data.distance);
+        ROS_INFO("MOVING FORWARD");
+    }
+
+    publisher.publish(move);
+}
+
+int main(int argc, char **argv) {
+    // Initialize a ROS node
+    ros::init(argc, argv, "motion_controller");
+
+    // Create a ROS NodeHandle object
+    ros::NodeHandle n;
+
+    // Define publisher on topic /cmd_vel
+    publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    // Define subscriber on topic /obstacle_detection
+    subscriber = n.subscribe<simple_robot_cpp::Obstacle>("/obstacle_detection", 10, callback);
+
+    // Keep the node in spin
+    ros::spin();
+
+    return 0;
+}
---
+++ simple_robot_cpp/src/obstacle_detector.cpp
@@ -0,0 +1,48 @@
+// obstacle_detector.cpp
+// Author: Mohamad Moudallal
+// GitHub: https://github.com/moudallal
+// Email: mohamad@moudallal.xyz
+
+#include "ros/ros.h"
+#include <vector>
+#include <algorithm>
+
+#include <sensor_msgs/LaserScan.h>
+#include "simple_robot_cpp/Obstacle.h"
+
+// Initialize the publisher and subscriber
+ros::Publisher publisher;
+ros::Subscriber subscriber;
+
+// Subscriber callback method
+void callback(const sensor_msgs::LaserScan data) {
+    // Access the forward laser scan distance (0-2pi -> ranges[0]-ranges[359])
+    std::vector<float> range;
+    range.insert(range.begin(), data.ranges.begin() + 354, data.ranges.end());
+    range.insert(range.end(), data.ranges.begin(), data.ranges.begin() + 5);
+    
+    simple_robot_cpp::Obstacle obstacle;
+    // Send the minimum distance in a 10-degree range for ensuring safety
+    obstacle.distance = *std::min_element(range.begin(), range.end());
+    // Send a dummy name
+    obstacle.name = "Obstacle";
+    publisher.publish(obstacle);
+}
+
+int main(int argc, char **argv) {
+    // Initialize a ROS node
+    ros::init(argc, argv, "obstacle_detector");
+
+    // Create a ROS NodeHandle object
+    ros::NodeHandle n;
+
+    // Define publisher on topic /obstacle_detection
+    publisher = n.advertise<simple_robot_cpp::Obstacle>("/obstacle_detection", 10);
+    // Define subscriber on topic /scan
+    subscriber = n.subscribe<sensor_msgs::LaserScan>("/scan", 10, callback);
+
+    // Keep the node in spin
+    ros::spin();
+
+    return 0;
+}
