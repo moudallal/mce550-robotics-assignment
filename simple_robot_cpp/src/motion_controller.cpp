10fab8e2236b509b66db4abc81249fa5e2a32b4b
Mohamad Moudallal <mohamad@moudallal.xyz>
2023-04-30 17:56:53 +0300

    Added basic velocity change functionality

--- simple_robot_cpp/src/motion_controller.cpp
+++ simple_robot_cpp/src/motion_controller.cpp
@@ -7,17 +7,21 @@
 
 #include "geometry_msgs/Twist.h"
 #include "simple_robot_msgs/Obstacle.h"
+#include "simple_robot_msgs/ChangeVelocity.h"
 
 // Initialize publisher and subscriber
 ros::Publisher publisher;
 ros::Subscriber subscriber;
 
+float linear_x = 0.3;
+float angular_z = 0.5;
+
 // Subscriber callback method
-void callback(const simple_robot_msgs::Obstacle data) {
+void obstacleCallback(const simple_robot_msgs::Obstacle data) {
     // Move the robot forward
     geometry_msgs::Twist move;
     move.angular.z = 0;
-    move.linear.x = 0.3;
+    move.linear.x = linear_x;
 
     // Evaluate the distance between the robot and the obstacle
     if (data.distance < 1) {
@@ -25,7 +29,7 @@ void callback(const simple_robot_msgs::Obstacle data) {
         ROS_INFO("SEARCHING FOR SAFE PATH");
         //  Rotate the robot to the left to find a safe path
         move.linear.x = 0;
-        move.angular.z = 0.5;
+        move.angular.z = angular_z;
     } else if (isinf(data.distance)) {
         ROS_INFO("No obstacles detected.");
         ROS_INFO("MOVING FORWARD");
@@ -35,6 +39,17 @@ void callback(const simple_robot_msgs::Obstacle data) {
     }
 
     publisher.publish(move);
+}
+
+bool handleChangeVelRequest(simple_robot_msgs::ChangeVelocity::Request& req, simple_robot_msgs::ChangeVelocity::Response& res) {
+    ROS_INFO("ChangeVelocity request received - linear_x: %1.2f, angular_z: %1.2f", (float)req.linear_x, (float)req.angular_z);
+
+    linear_x = req.linear_x;
+    angular_z = req.angular_z;
+
+    res.success = true;
+
+    return true;
 }
 
 int main(int argc, char **argv) {
@@ -47,7 +62,10 @@ int main(int argc, char **argv) {
     // Define publisher on topic /cmd_vel
     publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
     // Define subscriber on topic /obstacle_detection
-    subscriber = n.subscribe<simple_robot_msgs::Obstacle>("/obstacle_detection", 10, callback);
+    subscriber = n.subscribe<simple_robot_msgs::Obstacle>("/obstacle_detection", 10, obstacleCallback);
+
+    // Define service server on /simple_robot_cpp/change_vel
+    ros::ServiceServer change_vel_service = n.advertiseService("/simple_robot_cpp/change_vel", handleChangeVelRequest);
 
     // Keep the node in spin
     ros::spin();
